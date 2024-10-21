847f71f3ab2e49bed19d0fb8f494fd6ceb3bcdf1
tei4th1 <zhdanp95@mail.ru>
2024-10-21 17:38:03 +0300

    first commit

---
+++ ReactTo-Do/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+import './Header.css'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Plane Your Day</span>
+        </div>
+    </header>
+  )
+}
